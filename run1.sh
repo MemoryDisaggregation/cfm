f7b2e2e7ff18e0cfef9236668d04f4f229a2fbfb
YuqiLi <YuqiLi@localhost>
2024-11-21 19:22:58 -0700

    update

---
+++ run1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+program_path="/mydata/cfm/quicksort/quicksort"
+program_arg=10240
+
+cmd="taskset -c 0 /usr/bin/time -v $program_path $program_arg"
+
+
+echo $((5 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes
+
+$cmd &
+pid=$!
+
+sleep 0.5
+
+program_pid=$(pgrep -P $pid -n)
+if [ -z "$program_pid" ]; then
+    echo "Failed to find the actual program PID."
+    kill $taskset_pid 2>/dev/null
+    exit 1
+fi
+
+
+echo $program_pid | sudo tee /sys/fs/cgroup/memory/memctl/cgroup.procs
+
+echo "Program is running with PID: $program_pid"
+cat /sys/fs/cgroup/memory/memctl/memory.limit_in_bytes
+cat /sys/fs/cgroup/memory/memctl/cgroup.procs
+
+
+wait $pid
+exit_status=$?
---
+++ run2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+program_path="/mydata/cfm/quicksort/random_access"
+program_arg=32768
+
+cmd="taskset -c 1,2,3,4,5,6,7,8 /usr/bin/time -v $program_path $program_arg"
+
+
+echo $((16 * 1024 * 1024 * 1024)) | sudo tee /sys/fs/cgroup/memory/memctl2/memory.limit_in_bytes
+
+$cmd &
+pid=$!
+
+sleep 0.5
+
+program_pid=$(pgrep -P $pid -n)
+if [ -z "$program_pid" ]; then
+    echo "Failed to find the actual program PID."
+    kill $taskset_pid 2>/dev/null
+    exit 1
+fi
+
+
+echo $program_pid | sudo tee /sys/fs/cgroup/memory/memctl2/cgroup.procs
+
+echo "Program is running with PID: $program_pid"
+cat /sys/fs/cgroup/memory/memctl2/memory.limit_in_bytes
+cat /sys/fs/cgroup/memory/memctl2/cgroup.procs
+
+
+wait $pid
+exit_status=$?
